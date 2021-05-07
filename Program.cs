4d778ac591461d2b5655152991fd6c34760567f2
Muyiwa Olalekan <muyiwa.olalekan@thebulb.africa>
2021-05-07 16:36:56 +0100

    little change

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace HotelListing
         {
             Log.Logger = new LoggerConfiguration()
                 .WriteTo.File(
-                path: "C:\\hotellistings\\logs\\log-.txt",
+                path: "logs\\log-.txt",
                 outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}",
                 rollingInterval: RollingInterval.Day,
                 restrictedToMinimumLevel: LogEventLevel.Information
